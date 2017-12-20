db92b5a9f4b15e705f073fc076b2c41fde432be7
wonhyeongseo <pesfy101@gmail.com>
2017-12-20 14:21:48 +0900

    Make changes for linux-web

--- database_setup.py
+++ database_setup.py
@@ -54,7 +54,7 @@ class Item(Base):
         }
 
 
-engine = create_engine('sqlite:///catalog.db')
+engine = create_engine('postgresql://catalog:password@localhost/catalog')
 
 
 Base.metadata.create_all(engine)
