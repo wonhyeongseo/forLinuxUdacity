6eb6b27f5bb970284c1874af25bd2bfb0050afab
wonhyeongseo <pesfy101@gmail.com>
2017-12-20 14:52:46 +0900

    change to linux-web

--- fill_catalog.py
+++ fill_catalog.py
@@ -3,21 +3,20 @@ from sqlalchemy.orm import sessionmaker
 
 from database_setup import Category, Base, Item, User
 
-engine = create_engine('sqlite:///catalog.db')
-# Bind the engine to the metadata of the Base class so that the
-# declaratives can be accessed through a DBSession instance
+engine = create_engine('postgresql://catalog:password@localhost/catalog')
 Base.metadata.bind = engine
-
 DBSession = sessionmaker(bind=engine)
-# A DBSession() instance establishes all conversations with the database
-# and represents a "staging zone" for all the objects loaded into the
-# database session object. Any change made against the objects in the
-# session won't be persisted into the database until you call
-# session.commit(). If you're not happy about the changes, you can
-# revert all of them back to the last commit by calling
-# session.rollback()
 session = DBSession()
 
+# Delete all the categories to avoid repetition
+# session.query(Category).delete()
+# session.commit()
+
+# Delete all the items to avoid repetition
+# session.query(Item).delete()
+# session.commit()
+
+# Start new entries
 
 # Create dummy user
 User1 = User(name="Robo Barista", email="tinnyTim@udacity.com",
